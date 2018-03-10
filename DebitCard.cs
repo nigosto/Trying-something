5e0db5e810c2c3fe0cbd72dbc1c7ebbcc032aa4e
GeorgiAtanasov <nigosto@gmail.com>
2018-03-10 19:25:37 +0200

    w

---
+++ DebitCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DebitCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var thirdNumber = int.Parse(Console.ReadLine());
+            var fourthNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", firstNumber, secondNumber, thirdNumber, fourthNumber);
+        }
+    }
+}
